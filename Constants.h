9a1f9141f84c76035f4cbaea0f2a4fd74e736394
Silas A <agnewsilas@gmail.com>
2019-02-24 15:59:27 -0500

    Constants file

---
+++ Constants.h
@@ -0,0 +1,21 @@
+// Motor pins
+#define LEFT_PIN
+#define LEFT_HB1
+#define LEFT_HB2
+
+#define RIGHT_PIN
+#define RIGHT_HB1
+#define RIGHT_HB2
+
+#define SWEEPER_PIN
+
+// Sensors
+#define IR_LEFT
+#define IR_RIGHT
+#define SONAR_FRONT_TRIG
+#define SONAR_FRONT_ECHO
+
+// Constants
+#define P
+#define I
+#define D
